5b8f6f871fac55dc1e8dbecfe16539a1a5ac5727
Marina Adamova <marinaboykova8@gmail.com>
2023-05-21 10:43:27 +0300

    Add Homework7 - First Test Case

---
+++ src/main/java/Homework7/FirstTestCase.java
@@ -0,0 +1,54 @@
+package Homework7;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FirstTestCase extends generateRandomString {git
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // System Property for Chrome Driver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\marin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        // Launch website
+        driver.navigate().to("https://ultimateqa.com/");
+        driver.manage().window().maximize();
+
+        // Click one navigation link and verify the page url
+        WebElement navigationLink = driver.findElement(By.xpath("(//a[contains(.,'Professional Services')])[1]"));
+        navigationLink.click();
+        Assert.assertEquals("https://ultimateqa.com/consulting/", driver.getCurrentUrl());
+
+        // Locate page elements, Verify all elements are displayed
+        // Populate the data and click Send Message button
+        WebElement userName = driver.findElement(By.xpath("//input[@data-original_id='name']"));
+        Assert.assertTrue(userName.isDisplayed());
+        userName.sendKeys(generateRandomString());
+
+        WebElement email = driver.findElement(By.xpath("//input[contains(@data-original_id,'email')]"));
+        Assert.assertTrue(email.isDisplayed());
+        email.sendKeys(generateRandomString() + "@gmail.com");
+
+        WebElement companyName = driver.findElement(By.xpath("//input[contains(@placeholder,'Company Name')]"));
+        Assert.assertTrue(companyName.isDisplayed());
+        companyName.sendKeys(generateRandomString());
+
+        WebElement message = driver.findElement(By.xpath("//textarea[contains(@name,'et_pb_contact_message_0')]"));
+        Assert.assertTrue(message.isDisplayed());
+        message.sendKeys(generateRandomString());
+
+        WebElement sendMessageButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        Assert.assertTrue(sendMessageButton.isDisplayed());
+        sendMessageButton.click();
+
+        // Close the opened browser(s)
+        driver.quit();
+    }
+}
